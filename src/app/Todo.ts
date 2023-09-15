b615b587d4c8e918e023c2b986b1ab5631324132
Rohit Paradkar <rparadkar@technosofteng.com>
2023-09-15 18:37:06 +0530

    first commit

---
+++ src/app/Todo.ts
@@ -0,0 +1,6 @@
+export class Todo{
+    sno: number=1
+    title: string ="hello"
+    desc:string ="This "
+    active:boolean =false
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoComponent } from './MyComponent/todo/todo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+ 
